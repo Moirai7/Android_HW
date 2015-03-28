ba5139f268ea1f852f33f876222f0eb9044b339e
Moirai7 <emma@moirai.cn>
2015-03-28 08:44:40 +0800

    Update All dayu

--- client/app/src/main/java/com/moirai/view/RegisterActivity.java
+++ client/app/src/main/java/com/moirai/view/RegisterActivity.java
@@ -1,9 +1,19 @@
 package com.moirai.view;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.moirai.client.Config;
@@ -13,7 +23,10 @@ import com.moirai.client.R;
 import com.moirai.model.User;
 
 public class RegisterActivity extends BaseActivity {
-
+    private Button registerButton;
+    private EditText idEditText;
+    private EditText pwEditText;
+    private Spinner typeSpinner;
     @Override
     public void processMessage(Message message) {
         switch(message.what){
@@ -27,6 +40,8 @@ public class RegisterActivity extends BaseActivity {
                 else{
                     Toast.makeText(RegisterActivity.this, "注册失败", Toast.LENGTH_SHORT).show();
                     //TODO 注意编辑框的值要设置为空，像下面一样
+                    idEditText.setText("");
+                    pwEditText.setText("");
 //                    edit_username_reg.setText("");
 //                    edit_password1_reg.setText("");
 //                    edit_password2_reg.setText("");
@@ -42,12 +57,34 @@ public class RegisterActivity extends BaseActivity {
         setContentView(R.layout.activity_register);
 
         //TODO 得到编辑框里的值
-        //TODO 使用USER创建并调用register();
-        User user = new User();
-        user.setUsername("lanlan");
-        user.setPassword("123");
-        user.setType("1");
-        con.register(user);
-    }
+        registerButton = (Button) findViewById(R.id.register_createButton);
+        idEditText = (EditText) findViewById(R.id.register_id_EditView);
+        pwEditText = (EditText) findViewById(R.id.register_password1_EditView);
+        typeSpinner = (Spinner) findViewById(R.id.register_type);
 
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = idEditText.getText().toString();
+                String password = pwEditText.getText().toString();
+                String type =String.valueOf(typeSpinner.getSelectedItemPosition());
+                System.out.println("获取注册数据： "+id+password+type);
+
+                if(id == null || id == ""){
+
+                }else if(password == null || password == ""){
+
+                }else{
+                    //TODO 使用USER创建并调用register();
+                      User user = new User();
+                      user.setUsername(id);
+                      user.setPassword(password);
+                      user.setType(type);
+                      con.register(user);
+                }
+            }
+        });
+
+    }
 }
